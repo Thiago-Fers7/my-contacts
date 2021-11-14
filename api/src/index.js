aeb5a2a0d34ebfe344f02bd4d8fc355612962e0b
Luiz Thiago <thiago_fers7@hotamil.com>
2021-11-14 16:48:56 -0300

    config cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json()); // Middleware para receber body nas rotas
+app.use(cors);
 app.use(routes);
 
 // Error handler (middleware para tratar erros que o express detectar em todo o programa) para metódos não assíncronos
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
-app.listen(3000, () => console.log('Server started at http://locahost:3000'));
+app.listen(3333, () => console.log('Server started at http://locahost:3333'));
