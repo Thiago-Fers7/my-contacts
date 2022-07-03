f0860b42c72dc60be071820dd685cd7f8d2078ee
Luiz Thiago <thiago_fers7@hotmail.com>
2022-07-02 21:49:45 -0300

    Add event manager

--- app/src/components/Toast/ToastContainer/index.js
+++ app/src/components/Toast/ToastContainer/index.js
@@ -2,19 +2,23 @@ import { useEffect, useState } from 'react';
 import ToastMessage from '../ToastMessage';
 import { Container } from './styles';
 
+import { toastEventManager } from '../../../utils/toast';
+
 export default function ToastContainer() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    function handleAddToast(event) {
-      const { type, text } = event.detail;
-
+    function handleAddToast({ type, text }) {
       setMessages((prev) => [
         ...prev, { id: Math.random(), type, text },
       ]);
     }
 
-    document.addEventListener('addtoast', handleAddToast);
+    toastEventManager.on('addtoast', handleAddToast);
+
+    return () => {
+      toastEventManager.removeListener('addtoast', handleAddToast);
+    };
   }, []);
 
   return (
